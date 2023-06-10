5b6ae73aeb31840a36c939cc5edf53e401237795
ducdk <ducvuk6196@gmail.com>
2023-06-10 20:46:57 +0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,45 @@
+AOS.init();
+
+function playVideoBG(play = true) {
+    const modau_video1 = document.getElementById('modau_video1');
+    if (modau_video1) {
+        if (play) {
+            modau_video1.play();
+        } else {
+            modau_video1.pause();
+        }
+    }
+}
+
+function start() {
+    $('#load').hide();
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+    // $('#root-content').show();
+    playVideoBG(true);
+}
+
+$(window).on('load', function () {
+    console.log('Loaded');
+    $('#loading').hide();
+    $('#starting').show();
+});
+
+$(window).on('scroll', function () {
+    let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+
+    if (scrollTop > $(window).height()/2) {
+        $('.modau-1').css("opacity", 0);
+        playVideoBG(false);
+    } else {
+        $('.modau-1').css("opacity", 1);
+        playVideoBG(true);
+    }
+
+    let modau30 = $('.modau-30').height()
+    if (scrollTop > modau30  +   $(window).height() + 200) {
+        $('.modau-30').css("opacity", 0);
+    } else {
+        $('.modau-30').css("opacity", 1);
+    }
+});
+
